f85ac82bfc28c86330b88be9f803d4383c5f5c37
Aaron <48164620+aaronsang@users.noreply.github.com>
2023-05-17 12:13:13 -0400

    setup mongodb

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 // routes
-
 app.get("/", (req, res) => {
   res.send("Hello Node API");
 });
 
-app.listen(3000, () => {
-  console.log("Node API is running on port 3000...");
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://admin:r5s2k22Ux4FBec45q9@aaronrestfulapi.bewki4j.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB!");
+    app.listen(3000, () => {
+      console.log("Node API is running on port 3000...");
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB error", error);
+    process.exit(1);
+  });
