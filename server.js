66ff7a6d097baca119cc73fef22c86373b03b25b
Aaron <48164620+aaronsang@users.noreply.github.com>
2023-05-18 13:27:03 -0400

    making progress...

--- server.js
+++ server.js
@@ -1,21 +1,62 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Product = require("./models/productModel");
 
+app.use(express.json());
 // routes
 app.get("/", (req, res) => {
   res.send("Hello Node API");
 });
 
-app.post("/product", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.messsage);
+  }
+});
+
+// update a product
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id, req.body);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with id ${id}` });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose.set("strictQuery", false);
 mongoose
   .connect(
-    "mongodb+srv://admin:r5s2k22Ux4FBec45q9@aaronrestfulapi.bewki4j.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://aaroniscool:coolisaaron@aaronrestfulapi.bewki4j.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("Connected to MongoDB!");
